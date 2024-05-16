d7537365a6b2147c101dad3a0f6cef710a7ae525
Michael Phan <michaelphan35@gmail.com>
2024-05-16 13:46:15 -0700

    configured testing and first attempt at ci frontend pipeline

--- WOD/vite.config.ts
+++ WOD/vite.config.ts
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  test: {
+      coverage: {
+        provider: 'istanbul', // or 'v8'
+        reporter: ['text', 'json', 'html']
+      }
+  }
 })
