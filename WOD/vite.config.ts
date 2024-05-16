319395987e04d93ca17bcbc826924efaa7eb374e
Michael Phan <michaelphan35@gmail.com>
2024-05-16 14:51:48 -0700

    try 15

--- WOD/vite.config.ts
+++ WOD/vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
   test: {
       coverage: {
         provider: 'istanbul', // or 'v8'
-        reporter: ['text', 'json', 'html']
+        reporter: ['lcov', 'text', 'json', 'html']
       }
   }
 })
